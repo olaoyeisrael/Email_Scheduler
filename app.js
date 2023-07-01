c06ea7c11225048c4703ca841604d19ab2c3d92e
Israel <harduragbemeei@gmail.com>
2023-07-01 15:49:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const nodeCron = require('node-cron');
+const nodemailer = require('nodemailer');
+const {google} = require("googleapis")
+const config = require("./config.js")
+const OAuth2 = google.auth.OAuth2
+
+// const dotenv = require('dotenv')
+
+// dotenv.config()
+const OAuth2_client = new OAuth2(config.clientId, config.clientSecret)
+OAuth2_client.setCredentials({refresh_token: config.refreshToken})
+const accessToken = OAuth2_client.getAccessToken()
+
+app.use(express.json());
+
+// POST endpoint
+app.post('/send-email', (req, res) => {
+  const { email } = req.body;
+
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        type: 'OAuth2',
+        user: "harduragbemeei@gmail.com",
+        clientId: config.clientId,
+        clientSecret: config.clientSecret,
+        refreshToken: config.refreshToken, 
+        accessToken: accessToken
+      
+    },
+});
+
+
+
+   
+   
+  // creating CRON job to send email every 5 minutes
+  const Job = nodeCron.schedule('*/5 * * * *', () => {
+     // Generate six digit random code
+    const code = Math.floor(Math.random() * 90000 + 100000 )
+    //   console.log(code)
+    //   console.log(code.toString().length)
+
+    const mailOptions = {
+      from: config.user,
+      to: 'olaoyeaisrael@gmail.com',
+      subject: 'Code',
+      text: `Your six-digit code is: ${code}`
+    };
+
+    // Send email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
+  });
+
+  // Start the CRON job
+  Job.start();
+
+  res.send('Email scheduling started!');
+});
+
+// Start the server
+const port = 3000
+app.listen(port, () => {
+  console.log('server is running on port 3000');
+});
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,3 @@
+const code = Math.floor(Math.random() * 90000 + 100000 )
+console.log(code)
+console.log(code.toString().length)
